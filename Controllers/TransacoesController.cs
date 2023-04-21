56357bbb60ba6b12b26abd846457368247a321ca
Alana Peres <alanalimaperes@gmail.com>
2023-04-21 12:09:39 -0300

    Feat: Adicionando TransacoesController

---
+++ Controllers/TransacoesController.cs
@@ -0,0 +1,51 @@
+﻿using ApiSoftFinance.Context;
+using ApiSoftFinance.Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiSoftFinance.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransacoesController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public TransacoesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("{cpf}")]
+        public ActionResult<Transacao> Get(string cpf)
+        {
+            try
+            {
+                var transacao = _context.Transacoes.FirstOrDefault(c => c.Cpf == cpf);
+                if (transacao is null) return NotFound("Conta não encontrada");
+                return transacao;
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Ocorreu um erro ao processar sua requisição.");
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Post(Transacao transacao)
+        {
+            try
+            {
+                if (transacao is null) return BadRequest("Conta inválida.");
+                object value = _context.Transacoes.Add(transacao);
+                _context.SaveChanges();
+                return StatusCode(StatusCodes.Status201Created, transacao);
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, "Ocorreu um erro ao processar sua requisição.");
+            }
+        }
+    }
+}
