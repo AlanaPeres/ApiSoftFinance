65b45902e06da5cbc1bb36ae814431b94a491a7b
Lari <larileal@hotmail.com.br>
2023-04-23 13:46:53 -0300

    feat: configuracao do json e controller do login

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,58 @@
+﻿using ApiSoftFinance.Context;
+using ApiSoftFinance.JWTConfig;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiSoftFinance.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LoginController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public LoginController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        //[AllowAnonymous]
+        [Route("/logar")]
+        public async Task<IActionResult> Auth([FromBody] LoginRequest request)
+        {
+            try
+            {
+                var cliente = _context.Clientes.FirstOrDefault(c => c.Cpf == request.Cpf);
+                if (cliente is null) return NotFound("Cliente não encontrado");
+
+                if (cliente == null)
+                    return BadRequest(new { Message = "cpf VER e/ou senha está(ão) inválido(s)." });
+
+                if (cliente.Senha != request.Password)
+                    return BadRequest(new { Message = "cpf OUTRO e/ou senha está(ão) inválido(s)." });
+
+                var token = ConfigJwt.GenerateToken(cliente);
+
+                return Ok(new
+                {
+                    Token = token,
+                    Usuario = cliente,
+                });
+            }
+            catch (Exception)
+            {
+                return BadRequest(new { Message = "Ocorreu algum erro interno na aplicação, por favor tente novamente. " });
+            }
+        }
+
+
+        [HttpGet]
+        //[Authorize]
+        [Route("/autorizacao")]
+        public IActionResult GetAutorizacao([FromHeader] string Authorization)
+        {
+            return Ok(new { Authorization });
+        }
+    }
+}
---
+++ JWTConfig/ConfigJwt.cs
@@ -0,0 +1,34 @@
+﻿using ApiSoftFinance.Domain;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace ApiSoftFinance.JWTConfig
+{
+    public class ConfigJwt
+    {
+        public static string GenerateToken(Cliente cliente)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var key = Encoding.ASCII.GetBytes("ZWRpw6fDo28gZW0gY29tcHV0YWRvcmE=");
+
+            //informações do token: 
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+
+                }),
+                Expires = DateTime.UtcNow.AddHours(10),
+
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
